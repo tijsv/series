c48646e6cab5961c29b147cfb0223cee1d0a5742
tijsv <tijs.verellen@hotmail.com>
2018-02-21 20:06:20 +0100

    sorting

--- script.js
+++ script.js
@@ -2,22 +2,22 @@ $(function(){
 	// Array met alle series
 	// Index 2 staat voor de release date (srd = specific release date, rd = release date)
 	var series = [
-		["The Walking Dead", "images/twd.jpg", "srd", "26/02", "Season 7 Part 2"],
-		["Westworld", "images/westworld.jpeg", "srd", "22/04", "Season 2"],
-		["The 100", "images/the100.jpg", "srd", "24/04", "Season 5"],
-		["13 Reasons Why", "images/13reasonswhy.jpg", "rd", "2018", "Season 2"],
-		["Salvation", "images/salvation.jpg", "rd", "2018", "Season 2"],
-		["The OA", "images/theoa.jpg", "rd", " FEB 2018", "Season 2"],
-		["Stranger Things", "images/strangerthings.jpg", "rd", "2018", "Season 3"],
-		["Sense8", "images/sense8.jpg", "rd", "2018", "Season 2 Finale"],
-		["Ozark", "images/ozark.jpg", "rd", "2018", "Season 2"],
-		["Mindhunter", "images/mindhunter.jpg", "rd", "2018", "Season 2"],
-		["Vikings", "images/vikings.jpg", "rd", "2018", "Season 5 Part 2"],
-		["House Of Cards", "images/houseofcards.jpg", "rd", "2018", "Season 6"],
-		["The Expanse", "images/theexpanse.jpg", "rd", "2018", "Season 3"],
-		["Mars", "images/mars.jpg", "rd", "2018", "Season 2"],
-		["Game Of Thrones", "images/got.jpg", "rd", "2019", "Season 8"],
-		["Star Trek Discovery", "images/startrekdiscovery.jpg", "rd", "2019", "Season 2"],
+		["Game Of Thrones", "images/got.jpg", "rd", new Date(2019, 1, 0, 0, 0, 0), "Season 8"],
+		["Westworld", "images/westworld.jpeg", "srd", new Date(2018, 4, 22, 0, 0, 0), "Season 2"],
+		["The Walking Dead", "images/twd.jpg", "srd", new Date(2018, 2, 26, 0, 0, 0), "Season 7 Part 2"],
+		["The 100", "images/the100.jpg", "srd", new Date(2018, 4, 24, 0, 0, 0), "Season 5"],
+		["13 Reasons Why", "images/13reasonswhy.jpg", "rd", new Date(2018, 1, 0, 0, 0, 0), "Season 2"],
+		["Salvation", "images/salvation.jpg", "rd", new Date(2018, 1, 0, 0, 0, 0), "Season 2"],
+		["The OA", "images/theoa.jpg", "rd", new Date(2018, 2, 0, 0, 0, 0), "Season 2"],
+		["Stranger Things", "images/strangerthings.jpg", "rd", new Date(2018, 1, 0, 0, 0, 0), "Season 3"],
+		["Sense8", "images/sense8.jpg", "rd", new Date(2018, 1, 0, 0, 0, 0), "Season 2 Finale"],
+		["Ozark", "images/ozark.jpg", "rd", new Date(2018, 1, 0, 0, 0, 0), "Season 2"],
+		["Mindhunter", "images/mindhunter.jpg", "rd", new Date(2018, 1, 0, 0, 0, 0), "Season 2"],
+		["Vikings", "images/vikings.jpg", "rd", new Date(2018, 1, 0, 0, 0, 0), "Season 5 Part 2"],
+		["House Of Cards", "images/houseofcards.jpg", "rd", new Date(2018, 1, 0, 0, 0, 0), "Season 6"],
+		["The Expanse", "images/theexpanse.jpg", "rd", new Date(2018, 1, 0, 0, 0, 0), "Season 3"],
+		["Mars", "images/mars.jpg", "rd", new Date(2018, 1, 0, 0, 0, 0), "Season 2"],
+		["Star Trek Discovery", "images/startrekdiscovery.jpg", "rd", new Date(2019, 1, 0, 0, 0, 0), "Season 2"],
 		["Dark", "images/dark.jpg", "nrd", "????", "Season 2"],
 		["The Punisher", "images/thepunisher.jpg", "nrd", "????", "Season 2"],
 		["Travelers", "images/travelers.jpg", "nrd", "????", "Season 3"],
@@ -32,6 +32,7 @@ $(function(){
 	var rdSeries = [];
 	// Array met alle series waarvan een volgend seizoen nog bevestigd moet worden
 	var nrdSeries = [];
+	
 	// Forloop die alle series in de juiste array plaatst
 	for (var i = 0; i < series.length; i++) {
 		if (series[i][2] == "srd") {
@@ -42,6 +43,42 @@ $(function(){
 			nrdSeries.push(series[i]);
 		}
 	}
+
+	var sort_dates = function (date1, date2) {
+	  if (date1 > date2) return 1;
+	  if (date1 < date2) return -1;
+	  return 0;
+	};
+
+	function sortArray(array) {
+	  	var dates = [];
+		for (var i = 0; i < array.length; i++) {
+			dates.push(array[i][3]);
+		}
+		dates.sort(sort_dates);
+		var bin = [];
+		count = 0
+		for (var i = 0; i < dates.length; i++) {
+			for (var j = 0; j < array.length; j++) {
+				if (array[j][3] == dates[i]) {
+					bin.push(array[j]);
+					count =+ 1;
+				} 
+			}
+		}
+		array = bin;
+		return array;
+	}
+
+	srdSeries = sortArray(srdSeries);
+	rdSeries = sortArray(rdSeries);
+
+	// Array met de gesorteerde series
+	var seriesSorted = srdSeries + rdSeries + nrdSeries;
+	series = srdSeries.concat(rdSeries);
+	series = series.concat(nrdSeries);
+
+	var today = new Date();
 
 	for (var i = 0; i < series.length; i++) {
 		// Maak een serie-wrapper div aan
@@ -57,10 +94,25 @@ $(function(){
 		var newSerieTitle = document.createElement('h3');
 		newSerieTitle.innerHTML = series[i][0];
 		var newSerieRD = document.createElement('h4');
-		newSerieRD.innerHTML = series[i][3];
-
+		var newSerieThisMonth = document.createElement('h5');
+		if (series[i][2] == "srd") {
+			newSerieRD.innerHTML = series[i][3].getDate() + "/" + series[i][3].getMonth();
+			if (today.getFullYear() == series[i][3].getFullYear() && today.getMonth() + 1 == series[i][3].getMonth()) {
+				newSerieThisMonth.innerHTML = 'Soon';
+			}
+		} else if (series[i][2] == "rd") {
+			newSerieRD.innerHTML = series[i][3].getFullYear();
+		} else {
+			newSerieRD.innerHTML = series[i][3];
+		}
 		document.getElementById('main-all').appendChild(newSerieWrapper).appendChild(newSerie).appendChild(newSerieTitle);
 		document.getElementById(newSerie.id).appendChild(newSerieRD);
+		if (series[i][2] == "srd") {
+			if (today.getFullYear() == series[i][3].getFullYear() && today.getMonth() + 1 == series[i][3].getMonth()) {
+				document.getElementById(newSerie.id).appendChild(newSerieThisMonth);
+			}
+		}
+
 	}
 
 	// // Forloop die de elementen aanmaakt op de juiste plaats
